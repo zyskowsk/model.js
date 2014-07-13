1748dd2f899b7389cfd1bb225d9ee8c768a45be7
Rob Zyskowski <zyskowski.rob@gmail.com>
2014-07-12 22:10:17 -0700

    add attribues and fields to Model

--- test/model.js
+++ test/model.js
@@ -4,10 +4,41 @@ var model           = require_module('model');
 
 var Model           = model.Model;
 
-describe('Mode#_get_table_name', function() {
-    it('should be snake case version of model name', function() {
-        var TestModel = Model.create('TestModel');
+describe('Mode#_get_table_name', function () {
+    it('should be snake case version of model name', function () {
+        var TestModel = Model.create('TestModel', {});
         var test_model = new TestModel();
         test_model._get_table_name().should.equal('test_model');
+    });
+});
+
+describe('Model#Create', function () {
+    it('should set fields', function () {
+        var expected = ['name', 'text', 'number'];
+        var TestModel = Model.create('TestModel', {
+            name: 'String',
+            text: 'varchar(255)',
+            number: 'int'
+        });
+        var test_model = new TestModel();
+
+        test_model.fields.should.deep.equal(expected);
+    });
+
+    it('should set instance attributes', function () {
+        var TestModel = Model.create('TestModel', {
+            name: 'String',
+            text: 'varchar(255)',
+            number: 'int'
+        });
+        var test_model = new TestModel({
+            name: 'Rob',
+            text: 'hi',
+            number: 1
+        });
+
+        test_model.name.should.equal('Rob');
+        test_model.text.should.equal('hi');
+        test_model.number.should.equal(1);
     });
 });
