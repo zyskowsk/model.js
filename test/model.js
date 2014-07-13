2cd9bfa431306a44ff871dc0256c3c9324e01080
Rob Zyskowski <zyskowski.rob@gmail.com>
2014-07-12 21:16:34 -0700

    clean up/utilities/insert

--- test/model.js
+++ test/model.js
@@ -1,5 +1,8 @@
-var should = require('chai').should(),
-    Model = require('../model').Model;
+var require_module  = require('./helpers').require_module;
+var should          = require('chai').should();
+var model           = require_module('model');
+
+var Model           = model.Model;
 
 describe('Mode#_get_table_name', function() {
     it('should be snake case version of model name', function() {
